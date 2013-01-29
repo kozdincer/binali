b94b4517a59af8b397d1c3bcff753ca1e1f68259
K. Deniz Ogut <kdenizogut@gmail.com>
2013-01-29 22:19:56 +0200

    via http://m.internethaber.com/News.aspx?q=498790
    Binali Yıldırım'ın 29 Ocak 2013 tarihinde Ankara Sheraton Otel'de yeni hizmetini duyuran PTT'nin tanıtımında yaptığı konuşmanın http://m.internethaber.com/News.aspx?q=498790 adresindeki metninden K. Deniz Öğüt tarafından eklendi

--- binali.py
+++ binali.py
@@ -4,6 +4,9 @@
 QUOTES = (
     "Bu bilişime fazla kafayı yorarsan sıyırırsın.",
     "Bulut diye birşey çıkarmışlar, herkes içine birşey atıyor.",
+    "5 dakika 30 kuruş devam etsin. Ama bir de 2 dakika 10 kuruş, olursa bu iş tadından yenmez. Benimki sadece fikir.",
+    "PTT işin artmasından şikayet etmiyor.",
+    "(PTT Genel Müdürü'ne) Bırak bunu yemezler, hayır işini vakıflar yapar, [siz] ticarethanesiniz.",
 )
 
 import random
