87ad5a0a3314c27790ff534a8666ab5a56a9295d
Kaan Özdinçer <kaanozdincer@gmail.com>
2013-01-30 12:17:38 +0200

    Update binali.py
    Add more qutoes (Thanks to marenostrum for contribution)

--- binali.py
+++ binali.py
@@ -5,6 +5,8 @@ QUOTES = (
     "Bu bilişime fazla kafayı yorarsan sıyırırsın.",
     "Bulut diye birşey çıkarmışlar, herkes içine birşey atıyor.",
     "Sonra bahçesinde gençler kızlı, erkekli oturuyor. Ben çok şaşırdım. Burada yoldan çıkarım dedim. Benim okumam lazımdı.",
+    "5 dakika 30 kuruş devam etsin. Ama bir de 2 dakika 10 kuruş, olursa bu iş tadından yenmez. Benimki sadece fikir.",
+    "(PTT Genel Müdürü'ne) Bırak bunu yemezler, hayır işini vakıflar yapar, [siz] ticarethanesiniz.",
 )
 
 import random
