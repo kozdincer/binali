975e2394233ec1eab25ac9604e772747f7176089
Serdar Dalgıç <sd@serdardalgic.org>
2013-02-01 15:39:22 +0200

    Put the binali quotes into another file, it will be easier to write new quotes that way :)

---
+++ binali_quotes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+QUOTES = (
+    "Bulut diye birşey çıkarmışlar, herkes içine birşey atıyor.",
+
+    "Bu bilişim fazla kafa yorursan sıyırırsın... kullanacaksın, "
+    "nimetlerinden kullanıp, yararlanıp işini göreceksin, kafayı taktın mı "
+    "hıhı. o zaman işin kötü. çok fazla, hikmetine fazla şey yapmamak lazım.",
+
+    "binali in da sky with diamonds: https://vimeo.com/31575968",
+
+    "Sonra bahçesinde gençler kızlı, erkekli oturuyor. Ben çok şaşırdım. "
+    "Burada yoldan çıkarım dedim. Benim okumam lazımdı.",
+
+    "5 dakika 30 kuruş devam etsin. Ama bir de 2 dakika 10 kuruş, "
+    "olursa bu iş tadından yenmez. Benimki sadece fikir.",
+
+    "(PTT Genel Müdürü'ne) Bırak bunu yemezler, hayır işini vakıflar yapar, "
+    "[siz] ticarethanesiniz.",
+
+    "(Pegasus Havayollarının uygulamalarını yorumlarken) Oturduk para, "
+    "kalktık para, su içtik para, bedava hiçbir hizmet yok. "
+    "Bundan şaşmayın bu doğru.",
+
+    "(Hızlı tren kazası ardından) Valla fazla hızlandırmadık, sadece 16 km "
+    "hızlandırdık, sıfır kilometre araba bile alındığı gün kaza yapabilir.",
+
+    "Ama biz dedik ki (5 GB yapın). Herkes biraz daha fazla kullansın. "
+    "Film indirmek isteyen varsa onlar da kullansın. Onun için 3 GB'yi "
+    "5 GB'ye çıkardık, fiyatını 49 YTL'ye indirdik. Her şey düzeldi. "
+    "Yani bakan söz verdiyse yapar. Bu bakan öyle bir bakan.",
+)
