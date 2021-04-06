618f5d7c5cc96362e08c72eb24e4428658e4e9f4
beucismis <beucismis@tutamail.com>
2021-04-07 00:21:51 +0300

    Update binali_quotes.py

--- binali/binali_quotes.py
+++ binali/binali_quotes.py
@@ -3,78 +3,56 @@
 
 QUOTES = (
     "Bulut diye birşey çıkarmışlar, herkes içine bir şey atıyor.",
-
     "Bu bilişim fazla kafa yorarsan sıyırırsın... kullanacaksın, "
     "nimetlerinden kullanıp, yararlanıp işini göreceksin, kafayı taktın mı "
     "hıhı. o zaman işin kötü. çok fazla, hikmetine fazla şey yapmamak lazım.",
-
     "binali in da sky with diamonds: https://vimeo.com/31575968",
-
     "5 dakika 30 kuruş devam etsin. Ama bir de 2 dakika 10 kuruş, "
     "olursa bu iş tadından yenmez. Benimki sadece fikir.",
-
     "(PTT Genel Müdürü'ne) Bırak bunu yemezler, hayır işini vakıflar yapar, "
     "[siz] ticarethanesiniz.",
-
     "(Pegasus Havayolları'nın uygulamalarını yorumlarken) Oturduk para, "
     "kalktık para, su içtik para, bedava hiçbir hizmet yok. "
     "Bundan şaşmayın bu doğru.",
-
     "(Hızlı tren kazası ardından) Valla fazla hızlandırmadık, sadece 16 km "
     "hızlandırdık, sıfır kilometre araba bile alındığı gün kaza yapabilir.",
-
     "Ama biz dedik ki (5 GB yapın). Herkes biraz daha fazla kullansın. "
     "Film indirmek isteyen varsa onlar da kullansın. Onun için 3 GB'yi "
     "5 GB'ye çıkardık, fiyatını 49 YTL'ye indirdik. Her şey düzeldi. "
     "Yani bakan söz verdiyse yapar. Bu bakan öyle bir bakan.",
-
     "Boğaziçi Üniversitesi'ni ziyaret ettim. Bir baktım farklı bir dünya. "
     "Değişik binalar, surlarla çevrilmiş alan. Sonra bahçesinde gençler "
     "kızlı, erkekli oturuyor. Ben çok şaşırdım. Burada yoldan çıkarım dedim."
     "(27 Ocak 2013)",
-
     "(Twitter için) Bize afra, tafra yapmasınlar. Türkiye Cumhuriyeti'ni "
     "tanımayanı, Türkiye Cumhuriyeti de tanımaz. 76 milyon da onlara "
     "Osmanlı tokadını çakar.",
-
     "Sanal dünya yalan dünyası değildir.",
-
     "Uzaya güneş enerjisinden elektrik üreten güneş panelleri yerleştirip "
     "buradan üretilecek elektrik enerjisini radyo frekans dalgalarıyla "
     "yeryüzüne ulaştıracak bir projeyi de gerçekleştirmeyi hedefliyoruz. "
     "Bu da bir vizyon projesi olarak bu şurada kararlaştırılan bir "
     "çalışmadır.",
-
     "Çanakkale geçilmez, sözünün bizler için ne anlama geldiğini "
     "biliyorsunuz. O günlerde geçilemeyen Çanakkale, şimdi 4 bin metrelik "
     "asma köprüsüyle 4 dakikada geçilir hale gelecek. Bu projeyle de "
     "ülkemizin ulaşım ve iletişim altyapısı daha da farklı bir düzeye "
     "ulaştırılmış olacak",
-
     "Marmaray varken arabanın anlamı yok. Hâlâ arabayla gitmeye ısrar "
     "ederseniz o zaman şikayet etmeye hakkınız yok.",
-
     "Üsküdar’ı atı alan değil Marmaray bileti alan geçecek. (espriymiş)",
-
     "Marmaray denizin altında 62 metre derinliğinde. Yolculuk 3 dakika "
     "sürecek. İki lafın belini kıramadan geçeceksiniz. Güvenliği "
     "açısından hiçbir sıkıntı yok. Depreme karşı evinizden daha güvenli. "
     "İstanbul’da hangi evde oturursanız oturun depreme karşı o evden "
     "daha güvenli.",
-
-    "Asma köprü olur da sallanmaz mı? Ne yapacaktı yani, köprünün işi "
-    "sallanmak",
-
+    "Asma köprü olur da sallanmaz mı? Ne yapacaktı yani, köprünün işi " "sallanmak",
     "Marmaray açıldı halkımızın yüzde 80'i memnun. Açıldıktan sonra "
     "arıza oldu, yüzde 20'si memnun. Sonuçta yüzde 100'ü memnun.",
-
     "Bildiğiniz gibi bilgisayarlar Binali sistemiyle çalışır.",
-
     "Yolların kralı olmaz, yolların kuralı olur.",
-
     "Biliyorsunuz bilgisayarlar ikili sistemde çalışıyor, "
     "buna bina(r/ly) sistem deniyor.",
-
     "Ben çok rahatım canım, bir şey yok. Kazanın hızlandırılmış "
     "tren uygulamasıyla hiçbir alakası yok. Kazanın dar yarıçaplı "
     "kurpta aşırı hızdan olduğu açık ve nettir. Niye adam 80'le gitmesi "
@@ -83,23 +61,17 @@ QUOTES = (
     "Binali Yıldırım, Ulaştırma Bakanı, 38 kişinin öldüğü hızlandırılmış "
     "tren kazasından sonra bizlere bir kez daha yetki sahibi olduğunu "
     "fakat sorumluluk sahibi olmadığını hatırlatırken.",
-
     "Yasadışı dinlemeyi önlemenin tek yolu konuşmamak, "
     "konuştuğunuz sürece mutlaka dinleniyorsunuz. (3 Haziran 2008)",
-
     "Yolları bölüyoruz, hayatları birleştiriyoruz. Ülkeyi birbirine "
     "kaynaştırıyoruz. Burada üstad Aşık Veysel'e gönderme yapıldı, "
     "'Uzun ince bir yoldayım, gidiyorum gündüz gece' diye. "
     "Herhalde Aşık Veysel sağ olsaydı bu dizeyi, 'Uzun duble bir yoldayım,"
     " gidiyorum gündüz gece' diye söylerdi. (14 Haziran 2012)",
-
     "(İzmir mitinginde) İzmir'e hoş geldiniz. (16 Mart 2014)",
-
     "Milletimizin hafif bir ittirmesiyle iktidar olacağız inşallah",
-
     "Vurur yüze ifadesi, kullanıyorum sosyal medyayı bitanesi, eh eh eh eh. "
     "Videosu: https://www.youtube.com/watch?v=YXvxm0y3SME",
-
     "İnternet, çaya çorbaya limon gibi. Çok faydalı bir şey"
     "ama ayarını kaçırdığınız zaman ilaca benziyor, tedavi edecekken zehirliyor."
     "Gerçek hayatta ne suçsa, sanal dünyada da aynı şeyler suçtur."
@@ -108,15 +80,10 @@ QUOTES = (
     "Facebook'a bildirilen 417 içerikten 113'ü,"
     "Youtube'a bildirilen 297 içerikten 115'i çıkarılmadı."
     "Performans olarak en iyisi Facebook. (27 Şubat 2016)",
-    
     "Ulaşımda hız felaket, bilişimde hız berekettir.",
-    
     "Memleketin geleceği için bir Ali değil, Binali feda olsun.",
-    
     "İstanbul'da seçimi kazandık.",
-    
     "Bir insan düşünün, baş var, uzuvları var. "
     "Vatandaşa en yakın duran, vatandaşın her türlü sorunuyla derdiyle dertlenen birimler, "
-    "mahalli idareler, yerel yönetimlerdir. (8 Eylül 2016)"
-    
+    "mahalli idareler, yerel yönetimlerdir. (8 Eylül 2016)",
 )
