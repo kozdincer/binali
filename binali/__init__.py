2769d3b40aa32b22bdb49b3a1b1da141c8ec75cc
beucismis <beucismis@tutamail.com>
2021-04-07 00:13:05 +0300

    Update __init__.py

--- binali/__init__.py
+++ binali/__init__.py
@@ -1,16 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
 import random
 from argparse import ArgumentParser
-from binali_quotes import QUOTES as binaliQuotes
+from .binali_quotes import QUOTES as binaliQuotes
+
 
 parser = ArgumentParser(description="Echoes well-known quotes from binali.")
 # 29 is the index of "Binali feda olsun" quote.
-parser.add_argument('--feda', action='store_const', const=29)
+parser.add_argument("--feda", action="store_const", const=29)
 args = parser.parse_args()
 
-if args.feda is not None:
-	print(binaliQuotes[args.feda])
-else:
-	print(random.choice(binaliQuotes))
+
+def main():
+    if args.feda is not None:
+        print(binaliQuotes[args.feda])
+    else:
+        print(random.choice(binaliQuotes))
+
+
+if __name__ == "__main__":
+    main()
